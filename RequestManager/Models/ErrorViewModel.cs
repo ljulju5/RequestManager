b39ce9f97353262f1babea72b1f190cfb5618129
Ljubica Carkic <carkic5@gmail.com>
2021-10-28 20:57:05 +0200

    Add project files.

---
+++ RequestManager/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RequestManager.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
